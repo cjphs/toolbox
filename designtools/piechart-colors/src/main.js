c8a040470ce84f5405ab2dd10fae60f3d9379ba7
Christopher Schiffmann <72268429+cjphs@users.noreply.github.com>
2023-11-10 15:17:52 +0100

    copying values and some other stuff

--- designtools/piechart-colors/src/main.js
+++ designtools/piechart-colors/src/main.js
@@ -1,10 +1,17 @@
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('piechart');
 const colourPickerList = document.getElementById('colourPickerList')
+const colourListOutput = document.getElementById('colourListOutput')
 
 const slices = 30;
 let n = 12;
 const sliceSizes = Array.from({length: slices}, () => Math.random()*(n*=.75));
 sliceSizes.sort((a, b) => a - b).reverse()
+
+const defaultColours = [
+    '#47bbc1', '#fcc100', '#f44456', '#354052', '#4572A7',
+'#AA4643', '#89A54E', '#80699B', '#3D96AE', '#DB843D',
+'#92A8CD', '#A47D7C', '#B5CA92'//, 'grey' from here on...
+]
 
 
 const chart = new Chart(ctx, {
@@ -19,6 +26,18 @@ const chart = new Chart(ctx, {
     options: {
     }
 });
+
+let shiftHeld = false;
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Shift') {
+        shiftHeld = true
+    }
+})
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'Shift') {
+        shiftHeld = false
+    }
+})
 
 const appendColour = (colourValue='#d10a53') => {
     const colourPickerDiv = document.createElement('div')
@@ -42,6 +61,16 @@ const appendColour = (colourValue='#d10a53') => {
     const moveUpButton = document.createElement('button')
     moveUpButton.innerText = '↑'
     moveUpButton.addEventListener('click', () => {
+        if (shiftHeld) {
+            const colourPickerDivs = document.querySelectorAll('.colourPickerDiv')
+            const index = Array.from(colourPickerDivs).indexOf(colourPickerDiv)
+            if (index > 0) {
+                const nextColourPickerInput = colourPickerDivs[index - 1].querySelector('input')
+                nextColourPickerInput.value = colourPickerInput.value
+                nextColourPickerInput.dispatchEvent(new Event('change'))
+            }
+            return
+        }
         const colourPickerDivs = document.querySelectorAll('.colourPickerDiv')
         const index = Array.from(colourPickerDivs).indexOf(colourPickerDiv)
         if (index > 0) {
@@ -53,6 +82,16 @@ const appendColour = (colourValue='#d10a53') => {
     const moveDownButton = document.createElement('button')
     moveDownButton.innerText = '↓'
     moveDownButton.addEventListener('click', () => {
+        if (shiftHeld) {
+            const colourPickerDivs = document.querySelectorAll('.colourPickerDiv')
+            const index = Array.from(colourPickerDivs).indexOf(colourPickerDiv)
+            if (index < colourPickerDivs.length - 1) {
+                const nextColourPickerInput = colourPickerDivs[index + 1].querySelector('input')
+                nextColourPickerInput.value = colourPickerInput.value
+                nextColourPickerInput.dispatchEvent(new Event('change'))
+            }
+            return
+        }
         const colourPickerDivs = document.querySelectorAll('.colourPickerDiv')
         const index = Array.from(colourPickerDivs).indexOf(colourPickerDiv)
         if (index < colourPickerDivs.length - 1) {
@@ -81,6 +120,9 @@ const updateChart = () => {
     const chartColours = colours.concat(Array.from({length: slices - colours.length}, () => 'gray'))
     chart.data.datasets[0].backgroundColor = chartColours
     chart.update()
+
+    colourListOutput.textContent = colours.join(', ')
 }
 
+defaultColours.forEach(colour => appendColour(colour))
 updateChart()
